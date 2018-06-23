5f397519a0a6100eb4ce6886b7d60c79be6b6950
RafaelFRoque <rafael.roque@usp.br>
2018-06-23 15:19:19 -0300

    Classe base feita

---
+++ Assets/Scripts/PersonagemBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class PersonagemBase : MonoBehaviour {
+	protected int vida;
+	protected int velocidade;
+	
+
+	public virtual void PerdeVida(int dano){
+		this.vida -= dano;
+	}
+	public abstract void Move();
+}
