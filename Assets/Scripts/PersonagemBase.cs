db42eb9e51162526cb7eb24633f64edb71df4079
Oliver <oliverbecker@usp.br>
2018-06-23 21:29:51 +0000

    Player se movimenta e atualizado a classe base.
    Criada a classe para controlar o player e o prefab
    dele. Adicionada mais coisas à classe base.

--- Assets/Scripts/PersonagemBase.cs
+++ Assets/Scripts/PersonagemBase.cs
@@ -2,13 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-abstract class PersonagemBase : MonoBehaviour {
-	protected int vida;
-	protected int velocidade;
+abstract public class PersonagemBase : MonoBehaviour {
 	
+	[SerializeField] protected int health;
+	[SerializeField] protected int speed;
+	[SerializeField] protected float imunityCooldown;
 
-	public virtual void PerdeVida(int dano){
-		this.vida -= dano;
+	protected bool canMove;
+
+	private float imunityCounter;
+
+	void Start() {
+		imunityCounter = 0;
 	}
-	public abstract void Move();
+
+	void Update() {
+		if (imunityCounter < imunityCooldown)
+			imunityCounter += Time.deltaTime;
+	}
+
+	protected float GetImunityCounter() {
+		return imunityCounter;
+	}
+
+	public virtual void TakeDamage(int damage){
+		if (imunityCounter >= imunityCooldown) {
+			this.health -= damage;
+			imunityCounter = 0;
+		}
+	}
+
+	public abstract void Move ();
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PersonagemBase {
+
+	private Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = gameObject.GetComponent<Rigidbody2D> ();
+		if (rigid == null)
+			Debug.Log ("ERRO! Não existe Rigidbody2D neste GameObject");
+
+		canMove = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canMove)
+			Move ();
+	}
+
+	public override void Move() {
+		//Debug.Log ("movendo");
+		float horizontalMovement = Input.GetAxis ("Horizontal");
+		float verticalMovement = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (horizontalMovement, verticalMovement);
+
+		rigid.velocity = movement * speed;
+	}
+}
