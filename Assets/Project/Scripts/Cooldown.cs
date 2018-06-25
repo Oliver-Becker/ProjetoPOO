e647a5d987269e6a9aa6e0306ac3490c390748c0
Oliver <oliverbecker@usp.br>
2018-06-25 20:49:17 +0000

    Alterado o funcionamento do Cooldown.

---
+++ Assets/Project/Scripts/Cooldown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class CoRotina : MonoBehaviour {
+	public static CoRotina instance;
+
+	void Start() {
+		CoRotina.instance = this;
+	}
+}
+*/
+public class Cooldown {
+
+	public float cooldown;
+
+	public bool isReady {
+		get; private set;
+	}
+
+	public Cooldown() {
+		this.cooldown = 0;
+		this.isReady = true;
+	}
+
+	public Cooldown(float cooldown) {
+		this.cooldown = cooldown;
+		this.isReady = true;
+	}
+
+	public void StartCooldown(MonoBehaviour caller) {
+		caller.StartCoroutine (WaitCooldown ());
+	}
+
+	private IEnumerator WaitCooldown() {
+		isReady = false;
+		yield return new WaitForSeconds (cooldown);
+		isReady = true;
+	}
+}
--- Assets/Project/Scripts/PersonagemBase.cs
+++ Assets/Project/Scripts/PersonagemBase.cs
@@ -6,25 +6,28 @@ abstract public class PersonagemBase : MonoBehaviour {
 	
 	[SerializeField] protected int health;
 	[SerializeField] protected int speed;
-	[SerializeField] protected float immunityCooldown;
 
+	private Cooldown immunity = new Cooldown(1);
+
+	protected float immunityCooldown {
+		get { return immunityCooldown; }
+		set { 
+			if (value < 0)
+				immunity.cooldown = 0;
+			else
+				immunity.cooldown = value;
+		}
+	}
+			
 	protected bool canMove;
-	private bool isImmune = false;
+	protected bool canAttack;
 
 	public virtual void TakeDamage(int damage){
-		if (!isImmune) {
+		if (immunity.isReady) {
 			this.health -= damage;
-			StartCoroutine (WaitForImmunity ());
+			immunity.StartCooldown (this);
 		}
 	}
 
-	private IEnumerator WaitForImmunity() {
-		isImmune = true;
-		yield return new WaitForSeconds (immunityCooldown);
-		isImmune = false;
-	}
-
 	public abstract void Move ();
-
-
 }
